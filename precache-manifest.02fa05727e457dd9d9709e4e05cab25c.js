3ad1d7a09f350cfb24b947066f91f29d86dbd985
Davi Cecílio <davim.cecilio@gmail.com>
2020-06-22 19:39:04 -0300

    Updates

---
+++ precache-manifest.02fa05727e457dd9d9709e4e05cab25c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2568815d7c2bd422a691d27a8ec9544",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "968d99978594ef446586",
+    "url": "/tic-tac-toe/static/js/2.3c7b18cf.chunk.js"
+  },
+  {
+    "revision": "e30c39c3cd3a9ee70900efc9c36ff88d",
+    "url": "/tic-tac-toe/static/js/2.3c7b18cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92fd52098f810403eb86",
+    "url": "/tic-tac-toe/static/js/main.11e50781.chunk.js"
+  },
+  {
+    "revision": "ecbc6f5089b996663329",
+    "url": "/tic-tac-toe/static/js/runtime-main.a982f7cd.js"
+  },
+  {
+    "revision": "1801a3694b1faff4a369a310bc698cc1",
+    "url": "/tic-tac-toe/static/media/Calligraphr-Regular.1801a369.ttf"
+  },
+  {
+    "revision": "61e1350b19de2fef5f8a6f084fe40571",
+    "url": "/tic-tac-toe/static/media/backgroundRM.61e1350b.jpg"
+  },
+  {
+    "revision": "a6e693bfe5a0741e788f5e3f8fe39887",
+    "url": "/tic-tac-toe/static/media/divCustom.a6e693bf.svg"
+  },
+  {
+    "revision": "13d2b319b3279335635783011be8692b",
+    "url": "/tic-tac-toe/static/media/footerBackground.13d2b319.svg"
+  },
+  {
+    "revision": "4b4420cffe3ba4f13221e529e5b60078",
+    "url": "/tic-tac-toe/static/media/get_schwifty.4b4420cf.ttf"
+  },
+  {
+    "revision": "67536b53303a0349bfd8d80e2ded46f2",
+    "url": "/tic-tac-toe/static/media/logoDark.67536b53.png"
+  },
+  {
+    "revision": "5c2ac267e5490e00d97c6f6a112f77bb",
+    "url": "/tic-tac-toe/static/media/mortyPerson.5c2ac267.svg"
+  },
+  {
+    "revision": "ab9b0462a93b497b43c81911690c8359",
+    "url": "/tic-tac-toe/static/media/mortyWin.ab9b0462.gif"
+  },
+  {
+    "revision": "ef1c1f25e3519c07f753072a974ec167",
+    "url": "/tic-tac-toe/static/media/rickPerson.ef1c1f25.svg"
+  },
+  {
+    "revision": "a8174d0734f06c8089b06357ca7f6cdc",
+    "url": "/tic-tac-toe/static/media/rickWin.a8174d07.gif"
+  },
+  {
+    "revision": "c8f1fe23d59541f09ca9657643eebc63",
+    "url": "/tic-tac-toe/static/media/shittychan.c8f1fe23.ttf"
+  },
+  {
+    "revision": "b1e3ec3c5847f7093b6d40b359124d78",
+    "url": "/tic-tac-toe/static/media/tieWin.b1e3ec3c.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tic-tac-toe-hooks/precache-manifest.afeda02a86f5f3b87f0328caf94056b2.js"
+  "/tic-tac-toe/precache-manifest.02fa05727e457dd9d9709e4e05cab25c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tic-tac-toe-hooks/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tic-tac-toe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
