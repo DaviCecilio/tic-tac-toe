14932fc773cb91a5d460a2bd65f5bc56244e5887
Davi Cecílio <davim.cecilio@gmail.com>
2020-06-22 19:34:39 -0300

    Updates

---
+++ precache-manifest.afeda02a86f5f3b87f0328caf94056b2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e6462524c885a689a9a548199beeb51",
+    "url": "/tic-tac-toe-hooks/index.html"
+  },
+  {
+    "revision": "e11a958aff0c1a0c0c55",
+    "url": "/tic-tac-toe-hooks/static/js/2.0f06176b.chunk.js"
+  },
+  {
+    "revision": "e30c39c3cd3a9ee70900efc9c36ff88d",
+    "url": "/tic-tac-toe-hooks/static/js/2.0f06176b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfcf562d20637e60a773",
+    "url": "/tic-tac-toe-hooks/static/js/main.ad14676b.chunk.js"
+  },
+  {
+    "revision": "db8c31f754e72b9a0a3e",
+    "url": "/tic-tac-toe-hooks/static/js/runtime-main.b2a6d5e4.js"
+  },
+  {
+    "revision": "1801a3694b1faff4a369a310bc698cc1",
+    "url": "/tic-tac-toe-hooks/static/media/Calligraphr-Regular.1801a369.ttf"
+  },
+  {
+    "revision": "61e1350b19de2fef5f8a6f084fe40571",
+    "url": "/tic-tac-toe-hooks/static/media/backgroundRM.61e1350b.jpg"
+  },
+  {
+    "revision": "a6e693bfe5a0741e788f5e3f8fe39887",
+    "url": "/tic-tac-toe-hooks/static/media/divCustom.a6e693bf.svg"
+  },
+  {
+    "revision": "13d2b319b3279335635783011be8692b",
+    "url": "/tic-tac-toe-hooks/static/media/footerBackground.13d2b319.svg"
+  },
+  {
+    "revision": "4b4420cffe3ba4f13221e529e5b60078",
+    "url": "/tic-tac-toe-hooks/static/media/get_schwifty.4b4420cf.ttf"
+  },
+  {
+    "revision": "67536b53303a0349bfd8d80e2ded46f2",
+    "url": "/tic-tac-toe-hooks/static/media/logoDark.67536b53.png"
+  },
+  {
+    "revision": "5c2ac267e5490e00d97c6f6a112f77bb",
+    "url": "/tic-tac-toe-hooks/static/media/mortyPerson.5c2ac267.svg"
+  },
+  {
+    "revision": "ab9b0462a93b497b43c81911690c8359",
+    "url": "/tic-tac-toe-hooks/static/media/mortyWin.ab9b0462.gif"
+  },
+  {
+    "revision": "ef1c1f25e3519c07f753072a974ec167",
+    "url": "/tic-tac-toe-hooks/static/media/rickPerson.ef1c1f25.svg"
+  },
+  {
+    "revision": "a8174d0734f06c8089b06357ca7f6cdc",
+    "url": "/tic-tac-toe-hooks/static/media/rickWin.a8174d07.gif"
+  },
+  {
+    "revision": "c8f1fe23d59541f09ca9657643eebc63",
+    "url": "/tic-tac-toe-hooks/static/media/shittychan.c8f1fe23.ttf"
+  },
+  {
+    "revision": "b1e3ec3c5847f7093b6d40b359124d78",
+    "url": "/tic-tac-toe-hooks/static/media/tieWin.b1e3ec3c.gif"
+  }
+]);
